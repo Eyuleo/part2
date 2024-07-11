cd936d8144594cadb9b8a7a3018859afde62d548
Eyuleo <eyuleo10@gmail.com>
2024-07-11 20:31:31 +0300

    persist, update, delete contact using json-server with axios

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Person from './components/Person'
+import phonebook from './services/phonebook'
 
 const App = () => {
 	const [persons, setPersons] = useState([])
@@ -11,30 +12,52 @@ const App = () => {
 	const [filter, setFilter] = useState('')
 
 	useEffect(() => {
-		axios
-			.get('http://localhost:3000/persons')
-			.then((res) => setPersons(res.data))
+		phonebook.getAll().then((initialContacts) => setPersons(initialContacts))
 	}, [])
 
 	const addContact = (e) => {
 		e.preventDefault()
-		if (
-			persons.find(
-				(person) =>
-					person.name.toLocaleLowerCase() === newName.toLocaleLowerCase()
-			)
-		) {
-			alert(`${newName} is already added to phonebook`)
-			return
+		const existingContact = persons.find(
+			(person) =>
+				person.name.toLocaleLowerCase() === newName.toLocaleLowerCase()
+		)
+		if (existingContact) {
+			if (
+				confirm(
+					`${newName} is already added to phonebook, replace the old number with a new one?`
+				)
+			) {
+				const updatedContact = { ...existingContact, number: newPhone }
+				phonebook.update(existingContact.id, updatedContact).then((res) => {
+					setPersons(
+						persons.map((person) =>
+							person.id !== existingContact.id ? person : res
+						)
+					)
+				})
+				setNewName('')
+				setNewPhone('')
+			}
+		} else {
+			const newContact = {
+				name: newName,
+				number: newPhone,
+			}
+
+			phonebook.create(newContact).then((returnedObject) => {
+				setPersons(persons.concat(returnedObject))
+			})
+			setNewName('')
+			setNewPhone('')
 		}
-		const newContact = {
-			name: newName,
-			number: newPhone,
-			id: persons.length + 1,
+	}
+
+	const deleteContact = (id, name) => {
+		if (window.confirm(`Delete ${name}?`)) {
+			phonebook.deleteContact(id).then((returnedObject) => {
+				setPersons(persons.filter((person) => person.id !== id))
+			})
 		}
-		setPersons(persons.concat(newContact))
-		setNewName('')
-		setNewPhone('')
 	}
 
 	const contactToShow = persons.filter((person) => {
@@ -54,7 +77,7 @@ const App = () => {
 				setNewPhone={setNewPhone}
 			/>
 			<h2>Numbers</h2>
-			<Person contacts={contactToShow} />
+			<Person contacts={contactToShow} deleteContact={deleteContact} />
 		</div>
 	)
 }
--- phonebook/src/components/Person.jsx
+++ phonebook/src/components/Person.jsx
@@ -1,7 +1,10 @@
-const Person = ({ contacts }) => {
+const Person = ({ contacts, deleteContact }) => {
 	return contacts.map((contact) => (
 		<p key={contact.id}>
 			{contact.name} {contact.number}
+			<button onClick={() => deleteContact(contact.id, contact.name)}>
+				delete
+			</button>
 		</p>
 	))
 }
